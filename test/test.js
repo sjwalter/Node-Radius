0a2836242d96db8e791b53df6aa87dd72b43b3e0
Stephen Walters <stephenw@ssimicro.com>
2010-10-17 15:52:39 -0600

    Some more incremental progress

--- test/test.js
+++ test/test.js
@@ -1,9 +1,21 @@
-var radius = require('../lib/client'),
-    Dictionary = radius.Dictionary,
+var radius = require('../lib/client');
+
+var Dictionary = radius.Dictionary,
     Packet = radius.Packet,
     Client = radius.Cient;
 
 var d = new Dictionary({name: 'raddb', file: 'dictionary'});
 d.read(function() {
-    console.log(d.getAllAttributes());    
+    console.log('Successfully read dictionary. Contents:');
+    console.log(d.getAllAttributes());
+    
+    console.log('Instantiating new packet and adding attributes');
+    var p = new Packet(d, "foobarbaz");
+    p.addAttribute('hsa-uid', 40102);
+    p.addAttribute('foobar', 'baz');
+    
+    console.log('Added attributes, getting encoded attributes');
+    console.log(p.getEncodedAttributes().toString('base64'));
 });
+
+
